e389258eee3241279e982fbbfd7eabd92e7ab564
Stefan Caldararu <stefancaldararu@gmail.com>
2023-03-09 14:20:08 -0600

    add test file

---
+++ test.cpp
@@ -0,0 +1,16 @@
+//author: Stefan Caldararu
+
+//#include "mspace.h"
+#include "alg.h"
+#include <iostream>
+
+int main()
+{
+    Mspace mSpace;
+    mSpace.setSize(5);
+    mSpace.setDistance(1,1,25);
+    Alg algorithm(mSpace);
+    std::cout << algorithm.metricSpace.getDistance(1,1);
+
+    return 0;
+}
