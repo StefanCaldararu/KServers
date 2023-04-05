8bf6be55ea52d5e6f10cb631d22cea5edb5459a4
Stefan Caldararu <stefancaldararu@gmail.com>
2023-04-05 09:57:38 -0500

    write mcfp, still has error with Bellman-ford

--- RAII_Classes/mcfp.cpp
+++ RAII_Classes/mcfp.cpp
@@ -5,37 +5,151 @@
 
 #include <vector>
 #include "../mspace.h"
+#include<iostream>
+#include<stack>
+
 
 struct edge{
+    int src;
+    int dest;
     int cost;
     int flow;
+    edge(int s, int d, int c, int f){
+        src = s;
+        dest = d;
+        cost = c;
+        flow = f;
+    }
 };
 
 class Mcfp
 {
     private:
         std::vector<std::vector<edge> > graph;
+        int source;
+        int num_nodes;
+        int sink;
+        std::vector<int> s;
+        std::vector<int> r;
+        std::vector<int> rprime;
+        int k;
     public:
         Mcfp(){}
-        void setGraph(Mspace & metricSpace, std::vector<int> Sigma, int inputLength, int k){
+        void setGraph(Mspace & metricSpace, std::vector<int> Sigma, int inputLength, int num_servers, std::vector<int> init_config){
             graph.clear();
-            int num_nodes = 2+k+2*inputLength;
-            int source = 0;
-            int sink = num_nodes-1;
-            std::vector<int> s;
+            k = num_servers;
+            num_nodes = 2+k+2*inputLength;
+            source = 0;
+            sink = num_nodes-1;
+            s.clear();
+            r.clear();
+            rprime.clear();
             for(int i = 1; i<k+1; i++)
                 s.push_back(i);
-            std::vector<int> r;
-            std::vector<int> rprime;
             for(int i = 0;i<inputLength;i++){
                 r.push_back(k+1+2*i);
                 rprime.push_back(k+2+2*i);
             }
+            //Now want to make sure that graph is an adjacency matrix, with source, dest as index.
+            for(int i = 0;i<num_nodes;i++){
+                std::vector<edge> temp;
+                temp.reserve(num_nodes);
+                graph.push_back(temp);
+                for(int j = 0;j<num_nodes;j++)
+                    graph[i].push_back(edge(i,j,0,0));
+            }
+            //Now, we need to populate the graph as     
+            //Reference: in notes, 0404017, page 179/180. "NEW RESULTS ON SERVER PROBLEMS, M. CHROBAK"
+            for(int i = 0;i<k;i++){
+                graph[source][s[i]] = edge(source, s[i],0,1);
+                graph[s[i]][sink] = edge(s[i], sink, 0,1);
+                for(int j = 0;j<inputLength;j++)
+                    graph[s[i]][r[j]] = edge(s[i], r[j], metricSpace.getDistance(init_config[i],Sigma[j]),1);
+            }
+            for(int i = 0;i<inputLength;i++){
+                graph[rprime[i]][sink] = edge(rprime[i], sink, 0,1);
+                for(int j = i+1;j<inputLength;j++)
+                    graph[rprime[i]][r[j]] = edge(rprime[i], r[j], metricSpace.getDistance(Sigma[i], Sigma[j]), 1);
+                //FIXME: make this not hardcoded...
+                graph[r[i]][rprime[i]] = edge(r[i], rprime[i], -1e6,1);
+            }
             
+
         }
         int computeMCFP(){
-            return 0;
+            while(pathexists()){
+                std::vector<edge> path = findMinCostPath(source, sink);
+                reversePath(path);
+            }
+            //Now we have the residual graph. we want to do the same thing from the sink.
+            int cost = 0;
+            for(int i = 0;i<k;i++){
+                std::vector <edge> path = findMinCostPath(sink, s[i]);
+                for(int j = 0;j<path.size();j++)
+                    if(path[j].cost!=-1e6 && path[j].cost!=1e6)
+                        cost = cost+path[j].cost;
+            }
+            return cost;
+
         }
+        //check if a path exists from src to dest.
+        bool pathexists(){
+            for(int i = 0;i<k;i++)
+                if(graph[source][s[i]].flow == 1)
+                    return true;
+            return false;
+        }
+        //TODO: probably want to check this works...lol
+        std::vector<edge> findMinCostPath(int src, int dest){
+            std::vector<int> dist(num_nodes, 1e6);
+            std::vector<int> pred(num_nodes, -1);
+            dist[src] = 0;
+            for(int i = 0;i<num_nodes-1;i++){
+                for(int u = 0;u<num_nodes;u++){
+                    for(int v = 0;v<num_nodes;v++){
+                        if(graph[u][v].flow != 0 && (dist[u] != 1e6 && dist[u]+graph[u][v].cost < dist[v])){
+                            dist[v] = dist[u]+graph[u][v].cost;
+                            pred[v] = u;
+                        }
+                    }
+                }
+            }
+            //std::cout << "Shortest path from "<< src << " to " <<dest << ": ";
+            std::stack<int> path;
+            int node = dest;
+            while(node!=src){
+                path.push(node);
+                node = pred[node];
+            }
+            path.push(src);
+            // while(!path.empty()){
+            //     std::cout<< path.top()<< " ";
+            //     path.pop;
+            // }
+            // std::cout << std::endl;
+            std::vector<edge> ret;
+            int crnt_node = path.top();
+            path.pop();
+            while(!path.empty()){
+                ret.push_back(graph[crnt_node][path.top()]);
+                crnt_node = path.top();
+                path.pop();
+            }
+            std::cout << "Shortest distance from source to sink: " << dist[dest] << std::endl;
+
+            return ret;
+
+        }
+
+        void reversePath(std::vector<edge> path){
+            for(int i = 0;i<path.size();i++){
+                graph[path[i].dest][path[i].src] = graph[path[i].src][path[i].dest];
+                graph[path[i].src][path[i].dest] = edge(static_cast<int>(path[i].src), static_cast<int>(path[i].dest), 0,0);
+                if(graph[path[i].dest][path[i].src].cost == -1e6)
+                    graph[path[i].dest][path[i].src].cost =1e6;
+            }
+        }
+
         ~Mcfp(){}
 };
 
--- test.cpp
+++ test.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <boost/graph/adjacency_list.hpp>
-
-using namespace boost;
-
+#include "RAII_Classes/mcfp.cpp"
+#include "mspace.h"
+#include<vector>
 int main() {
-    // Define the graph using an adjacency_list
-    typedef adjacency_list<vecS, vecS, directedS, no_property, property<edge_weight_t, int, property<edge_cost_t, int>>> Graph;
+    Mspace metricSpace;
+    metricSpace.setSize(3);
+    metricSpace.setDistance(0,0,0);
+    metricSpace.setDistance(0,1,3);
+    metricSpace.setDistance(0,2,2);
+    metricSpace.setDistance(1,0,3);
+    metricSpace.setDistance(1,1,0);
+    metricSpace.setDistance(1,2,4);
+    metricSpace.setDistance(2,0,2);
+    metricSpace.setDistance(2,1,4);
+    metricSpace.setDistance(2,2,0);
 
-    // Define the edge properties
-    typedef property_map<Graph, edge_weight_t>::type WeightMap;
-    typedef property_map<Graph, edge_cost_t>::type CostMap;
-
-    // Define the vertices and edges of the graph
-    const int num_vertices = 4;
-    enum { S, A, B, T };
-    Graph g(num_vertices);
-    WeightMap weight = get(edge_weight, g);
-    CostMap cost = get(edge_cost, g);
-
-    // Add the edges to the graph
-    add_edge(S, A, {2, 3}, g); // Edge from S to A with weight 2 and cost 3
-    add_edge(S, B, {1, 4}, g); // Edge from S to B with weight 1 and cost 4
-    add_edge(A, B, {1, 2}, g); // Edge from A to B with weight 1 and cost 2
-    add_edge(A, T, {1, 5}, g); // Edge from A to T with weight 1 and cost 5
-    add_edge(B, T, {2, 1}, g); // Edge from B to T with weight 2 and cost 1
-
-    // Print the edges of the graph with their weights and costs
-    graph_traits<Graph>::edge_iterator e, e_end;
-    for (tie(e, e_end) = edges(g); e != e_end; ++e) {
-        std::cout << "Edge (" << source(*e, g) << ", " << target(*e, g) << ") with weight " << weight[*e] << " and cost " << cost[*e] << std::endl;
-    }
-
+    std::vector<int> Sigma;
+    Sigma.push_back(2);
+    Sigma.push_back(0);
+    Sigma.push_back(2);
+    int inputLength = 3;
+    int num_servers = 2;
+    std::vector<int> init_config;
+    init_config.push_back(0);
+    init_config.push_back(1);
+    Mcfp calc;
+    calc.setGraph(metricSpace, Sigma, inputLength, num_servers, init_config);
+    int cost = calc.computeMCFP();
+    std::cout << cost << std::endl;
     return 0;
 }
